f555877f128070c01b8e8d0e47bacf7bf54fce6f
Regis Ximendes <regis.ximendes@ifood.com.br>
2021-07-28 16:03:19 -0300

    Make feed structure

--- app/src/main/java/com/example/composelab/MainActivity.kt
+++ app/src/main/java/com/example/composelab/MainActivity.kt
@@ -3,11 +3,20 @@ package com.example.composelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composelab.ui.theme.ComposeLabTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +26,7 @@ class MainActivity : ComponentActivity() {
             ComposeLabTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    BuildCard()
                 }
             }
         }
@@ -25,14 +34,59 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun BuildCard() {
+
+    Column{
+        Row(
+            modifier = Modifier.padding(20.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_user_avatar),
+                contentDescription = null,
+                modifier = Modifier
+                    .clip(shape = CircleShape)
+                    .size(32.dp)
+            )
+
+           Column(
+               modifier = Modifier.padding(start = 12.dp)
+           ) {
+               Text(text = "Novo post titulo")
+               Text(text = "Aberto Agora")
+           }
+        }
+        Row {
+            Image(
+                painter = painterResource(id = R.drawable.ic_user_avatar),
+                contentDescription = null,
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.Crop
+            )
+        }
+        Row(
+            modifier = Modifier.padding(20.dp)
+        ) {
+            Column {
+                Text(text = "Titulo do produto")
+                Row {
+                    Text(text = "R$ 19,90")
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Text(text = "R$ 25,90")
+                }
+            }
+            Image(
+                painter = painterResource(id = R.drawable.ic_user_avatar),
+                contentDescription = null,
+                modifier = Modifier.size(24.dp)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeLabTheme {
-        Greeting("Android")
+        BuildCard()
     }
 }
