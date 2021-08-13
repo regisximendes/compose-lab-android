1a3046d0a10e3556c83789454f27b1478660aef6
Victor Magosso <inv.vmagosso@ifood3rd.com>
2021-08-13 16:12:34 -0300

    Logic when there is no offer price

--- app/src/main/java/com/example/composelab/feed/presentation/FeedActivity.kt
+++ app/src/main/java/com/example/composelab/feed/presentation/FeedActivity.kt
@@ -3,10 +3,16 @@ package com.example.composelab.feed.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
 import com.example.composelab.feed.domain.FeedPost
 import com.example.composelab.feed.domain.FeedPostFactory
 import com.example.composelab.theme.ComposeLabTheme
@@ -26,6 +32,18 @@ class FeedActivity : ComponentActivity() {
                             }
                         }
                     }
+
+        /**exemplo com foreach*/
+//                    Column(modifier = Modifier
+//                        .fillMaxWidth()
+//                        .verticalScroll(ScrollState(0))) {
+//                        FeedPostFactory.makeFeedPostList(10).forEach { post ->
+//                            when (post) {
+//                                is FeedPost.TextPost -> FeedTextCard(textPost = post)
+//                                is FeedPost.ImagePost -> FeedImageCard(imagePost = post)
+//                            }
+//                        }
+//                    }
                 }
             }
         }
--- app/src/main/java/com/example/composelab/feed/presentation/FeedCardLayout.kt
+++ app/src/main/java/com/example/composelab/feed/presentation/FeedCardLayout.kt
@@ -4,15 +4,23 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.LocalTextStyle
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.example.composelab.feed.domain.Merchant
@@ -56,6 +64,9 @@ fun FeedCardHeader(merchant: Merchant) {
 
 @Composable
 fun FeedCardFooter(product: Product) {
+    val currentPrice = if (product.price.isEmpty())
+        product.originalPrice else product.price
+
     Row(
         modifier = Modifier
             .padding(16.dp)
@@ -68,15 +79,56 @@ fun FeedCardFooter(product: Product) {
         ) {
             Text(text = product.name)
             Row {
-                Text(text = product.price)
+                Text(text = currentPrice)
                 Spacer(modifier = Modifier.width(8.dp))
-                Text(
+                TextOrGone(
                     color = Color.LightGray,
                     text = product.originalPrice,
                     style = TextStyle(textDecoration = TextDecoration.LineThrough)
                 )
             }
         }
+    }
+}
+
+@Composable
+fun TextOrGone(
+    text: String,
+    modifier: Modifier = Modifier,
+    color: Color = Color.Unspecified,
+    fontSize: TextUnit = TextUnit.Unspecified,
+    fontStyle: FontStyle? = null,
+    fontWeight: FontWeight? = null,
+    fontFamily: FontFamily? = null,
+    letterSpacing: TextUnit = TextUnit.Unspecified,
+    textDecoration: TextDecoration? = null,
+    textAlign: TextAlign? = null,
+    lineHeight: TextUnit = TextUnit.Unspecified,
+    overflow: TextOverflow = TextOverflow.Clip,
+    softWrap: Boolean = true,
+    maxLines: Int = Int.MAX_VALUE,
+    onTextLayout: (TextLayoutResult) -> Unit = {},
+    style: TextStyle = LocalTextStyle.current
+) {
+    if (text.isNotEmpty()) {
+        Text(
+            text = text,
+            modifier = modifier,
+            onTextLayout = onTextLayout,
+            color = color,
+            fontSize = fontSize,
+            fontStyle = fontStyle,
+            fontWeight = fontWeight,
+            fontFamily = fontFamily,
+            letterSpacing = letterSpacing,
+            textDecoration = textDecoration,
+            textAlign = textAlign,
+            lineHeight = lineHeight,
+            overflow = overflow,
+            softWrap = softWrap,
+            maxLines = maxLines,
+            style = style
+        )
     }
 }
 
